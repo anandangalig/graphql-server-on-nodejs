42b7878d19f9af43945cd587d94a2db3edc4ee97
Anand Angalig <anandangalig@gmail.com>
2019-06-07 10:46:20 -0400

    hook up Prisma to GraphQL server context: WIP only feed and post working with Prisma at this point; generated prisma client

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 const { GraphQLServer } = require('graphql-yoga');
+const { prisma } = require('./generated/prisma-client');
 
-let links = [
-  {
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL',
-  },
-];
-let idCount = links.length;
 let findLinkIndex = id => {
   return links.findIndex(link => {
     return link.id === id;
   });
 };
 // resolvers:
+//GraphQL resolver functions always receive four arguments: root, args, context, info
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
+    feed: (root, args, context, info) => {
+      // context: plain JavaScript object that every resolver in the resolver chain can read from and write to - it thus basically is a means for resolvers to communicate. Also, a way to pass arbitrary data or functions to the resolvers
+      return context.prisma.links();
+      // This Prisma client instance effectively lets you access your database through the Prisma API. It exposes a number of methods that let you perform CRUD operations for your models.
+    },
     link: (parent, args) => {
       const linksMatched = links.filter(link => {
         return link.id === args.id;
@@ -26,16 +24,12 @@ const resolvers = {
     },
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
+    post: (root, args, context) => {
+      return context.prisma.createLink({
         url: args.url,
-      };
-      links.push(link);
-      console.log('POST: ', links);
-
-      return link;
+        description: args.description,
+      });
+      // Prisma client exposes a CRUD API for the models in your datamodel for you to read and write in your database. These methods are auto-generated based on model definitions in datamodel.prisma.
     },
     updateLink: (parent, args) => {
       const targetLinkIndex = findLinkIndex(args.id);
@@ -63,5 +57,6 @@ const resolvers = {
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: { prisma },
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
