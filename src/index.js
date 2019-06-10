0490cf4db78a112cbc32c687064bd6a2a9928cfd
Anand Angalig <anandangalig@gmail.com>
2019-06-10 11:41:11 -0400

    extend subscription to include Vote per link

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
 const User = require('./resolvers/User');
 const Link = require('./resolvers/Link');
+const Vote = require('./resolvers/Vote');
 const Subscription = require('./resolvers/Subscription');
 
 // resolvers:
@@ -13,6 +14,7 @@ const resolvers = {
   Subscription,
   User,
   Link,
+  Vote,
 };
 
 // create server
--- src/resolvers/Link.js
+++ src/resolvers/Link.js
@@ -3,7 +3,10 @@ function postedBy(parent, args, context) {
   // WHAT IS HAPPENING HERE?
   // .postedBy() call cannot be a recursion, right?
 }
-
+function votes(parent, args, context) {
+  return context.prisma.link({ id: parent.id }).votes();
+}
 module.exports = {
   postedBy,
+  votes,
 };
