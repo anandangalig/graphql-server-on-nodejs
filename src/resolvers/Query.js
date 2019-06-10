0ecc4b1e3c1ab8bf30aac5583e3ede21c8e47450
Anand Angalig <anandangalig@gmail.com>
2019-06-10 12:17:13 -0400

    add filtering and pagination to Link feed

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,15 @@
-function feed(parent, args, context, info) {
-  return context.prisma.links();
+async function feed(parent, args, context, info) {
+  const where = args.filter
+    ? { OR: [{ description_contains: args.filter }, { url_contains: args.filter }] }
+    : {};
+
+  const links = await context.prisma.links({
+    where,
+    skip: args.skip,
+    first: args.first,
+  });
+
+  return links;
 }
 function link(root, { id }, context, info) {
   return context.prisma.link({ id });
